8f274227cabc7019e3b855a0e175ea9dbf272ee9
gaqlaxy <v4vripper@gmail.com>
2024-10-24 16:28:19 +0530

    Added Skills

---
+++ vite-project/src/Skills.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+export default function Skills() {
+  return (
+    <>
+      <div className="">
+        <div>
+          <div className="skils my-5 mx-24">
+            <div class="skills bg-slate-50 p-5 text-sm tracking-wider font-normal ">
+              <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                <p class="text-sm">Skill 1</p>
+                <p class="text-sm">90%</p>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                  {" "}
+                  90%
+                </div>
+              </div>
+              <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                <p class="text-sm">Skill 2</p>
+                <p class="text-sm">85%</p>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                  {" "}
+                  85%
+                </div>
+              </div>
+              <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                <p class="text-sm">Skill 3</p>
+                <p class="text-sm">75%</p>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                  {" "}
+                  75%
+                </div>
+              </div>
+              <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                <p class="text-sm">Skill 4</p>
+                <p class="text-sm">85%</p>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                  {" "}
+                  85%
+                </div>
+              </div>
+              <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                <p class="text-sm">Skill 5</p>
+                <p class="text-sm">90%</p>
+              </div>
+              <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                  {" "}
+                  90%
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* <div>
+                <div className="skils my-5 mx-24">
+                  <div class="skills bg-slate-50 p-5 text-sm tracking-wider font-normal ">
+                    <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                      <p class="text-sm">Skill 1</p>
+                      <p class="text-sm">90%</p>
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                      <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                        {" "}
+                        90%
+                      </div>
+                    </div>
+                    <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                      <p class="text-sm">Skill 2</p>
+                      <p class="text-sm">85%</p>
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                      <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                        {" "}
+                        85%
+                      </div>
+                    </div>
+                    <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                      <p class="text-sm">Skill 3</p>
+                      <p class="text-sm">75%</p>
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                      <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                        {" "}
+                        75%
+                      </div>
+                    </div>
+                    <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                      <p class="text-sm">Skill 4</p>
+                      <p class="text-sm">85%</p>
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                      <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                        {" "}
+                        85%
+                      </div>
+                    </div>
+                    <div class="mt-5 mb-2 mx-3 flex items-center justify-between gap-5 ">
+                      <p class="text-sm">Skill 5</p>
+                      <p class="text-sm">90%</p>
+                    </div>
+                    <div className="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                      <div className="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full">
+                        {" "}
+                        90%
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
+      </div>
+    </>
+  );
+}
